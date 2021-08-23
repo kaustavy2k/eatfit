910a7d7733c6c8553b44659b1aed948df3dc5c37
kaustav basak <kaustavbasaky2k@gmail.com>
2021-08-23 18:35:51 +0530

    bugs

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 app.use("/", routes);
+app.use(express.static(__dirname + "/client/build"));
+const path = require("path");
+app.get("*", (req, res) => {
+  res.status(200).sendFile("index.html", { root: __dirname });
+});
 module.exports = app;
